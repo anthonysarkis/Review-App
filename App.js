c7ca7c98c5bf043b818f3a466d28fd8d959702bb
Anthony Sarkis <sarkis_anthony@hotmail.com>
2020-07-30 20:15:01 -0400

    fonts added

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,  } from 'react-native';
 import ReviewDetails from './screens/reviewDetails';
 import Home from './screens/home';
 import About from './screens/about';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const getFonts = () => {
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Home></Home>
-      <About></About>
-      <ReviewDetails></ReviewDetails>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Home></Home>
+        <About></About>
+        <ReviewDetails></ReviewDetails>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  else {
+    return(
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
